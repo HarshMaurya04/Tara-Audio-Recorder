31d73eef03d833faaa8831add566efd089ff411a
HarshMaurya04 <harshm8540@gmail.com>
2026-02-18 16:25:45 +0530

    -

--- src/services/botExtension.js
+++ src/services/botExtension.js
@@ -2,23 +2,13 @@ export const getSenderFromBot = () => {
   return new Promise((resolve) => {
     if (window.BotExtension) {
       window.BotExtension.getPayload((data) => {
-        console.log("Raw payload from SwiftChat:", data);
-
-        // Handle both formats safely
-        if (typeof data === "string") {
-          resolve(data);
-        } else if (data?.value) {
-          resolve(data.value);
-        } else {
-          resolve(null);
-        }
+        resolve(data.value);
       });
     } else {
       resolve(null);
     }
   });
 };
-
 
 export const closeWebView = () => {
   if (window.BotExtension) {
